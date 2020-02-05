438a1d2d263fbf2d66f36b79059e69662be57bc5
Joaquin Gumucio L <jjgumucio@gmail.com>
2020-02-05 12:46:44 -0300

    Finaliza maqueta sección noticias

--- src/pages/Home/News/index.tsx
+++ src/pages/Home/News/index.tsx
@@ -1,60 +1,62 @@
 import React from 'react'
 import Title from 'components/Title'
-import DateSubscriber from 'components/Events/DateSubscriber'
 import DataRow from 'components/Events/DataRow'
 import ViewMoreButton from 'components/ViewMoreButton'
-import { List, Icon } from 'antd'
+import { List, Icon, Typography } from 'antd'
 import styles from './styles.module.sass'
 
-const Events: React.FC = () => {
-  const events = [
+const { Text } = Typography
+
+const News: React.FC = () => {
+  const news = [
     {
-      date: '2020-02-07',
-      time: '19:30',
-      title: 'Taller de Teoría y Solfeo',
-      description: 'Aprende teoría musica y solfeo. Mejora tu interpretación y lectura musical',
-      imageUrl: 'asdfasdfas',
-      address: { streetName: 'Centro Cultural Baquedano', streetNumber: '' },
+      id: 'afs24fasdf24f',
+      title: 'Rancagua primera ciudad con PWA',
+      subtitle: 'Rancagua lidera el uso de tecnologías en la interacción con los ciudadanos.',
+      body: 'Rancagua lidera el uso de tecnologías. Rancagua primera ciudad con PWA',
+      publishedAt: '2020-02-07',
+      departmentId: 'fDadfWb32',
+      tags: ['tecnología', 'internet'],
+      imageUrl: 'https://rancagua.cl/plataforma/upload/fotos/389785d677ac78a18cedff122c8ec816.jpg',
       externalUrl: 'https://google.cl',
-      isFree: true,
     },
     {
-      date: '2020-02-20',
-      time: '16:00',
-      title: 'Taller Cultivando Nuestro Patrimonio Natural',
-      description: 'Aprende teoría musica y solfeo. Mejora tu interpretación y lectura musical',
-      imageUrl: 'asdfasdfas',
-      address: { streetName: 'Centro Ambiental', streetNumber: '' },
+      id: 'afs24fasdf24f',
+      title: 'Rancagua entrega noticias',
+      subtitle:
+        'A través de esta sección te enterarás de todo lo que ocurre en la comuna de Rancagua.',
+      body: 'Rancagua entrega noticias: A través de esta sección te enterarás de todo',
+      publishedAt: '2020-02-14',
+      departmentId: 'dsCwRG0148',
+      imageUrl: 'https://rancagua.cl/plataforma/upload/fotos/389785d677ac78a18cedff122c8ec816.jpg',
+      tags: ['noticias', 'actualidad'],
       externalUrl: 'https://google.cl',
-      isFree: false,
     },
     {
-      date: '2020-03-08',
-      time: '20:30',
-      title: 'Tercera Corrida Familiar',
-      description: 'Aprende teoría musica y solfeo. Mejora tu interpretación y lectura musical',
-      imageUrl: 'asdfasdfas',
-      address: {
-        streetName: 'Centro Deportivo Patricio Mekis',
-        streetNumber: '',
-      },
+      id: 'afs24fasdf24f',
+      title: 'Rancagua entrega noticias',
+      subtitle: 'A través de esta sección te enterarás de todo',
+      body: 'Rancagua entrega noticias: A través de esta sección te enterarás de todo',
+      publishedAt: '2020-02-14',
+      departmentId: 'dsCwRG0148',
+      imageUrl: 'https://rancagua.cl/plataforma/upload/fotos/389785d677ac78a18cedff122c8ec816.jpg',
+      tags: ['noticias', 'actualidad'],
       externalUrl: 'https://google.cl',
-      isFree: true,
     },
   ]
 
   return (
-    <section className={styles.events}>
+    <section className={styles.news}>
       <Title>NOTICIAS</Title>
 
       <img
-        className={styles.eventsImg}
+        className={styles.newsImg}
         src='https://rancagua.cl/plataforma/upload/fotos/389785d677ac78a18cedff122c8ec816.jpg'
-        alt={events[0].title}
+        alt={news[0].title}
       />
       <List
-        className='eventsList'
-        dataSource={events}
+        className='newsList'
+        dataSource={news}
         renderItem={item => (
           <List.Item actions={[<Icon type='right' />]}>
             <a className={styles.linkItem} href={item.externalUrl}>
@@ -62,17 +64,8 @@ const Events: React.FC = () => {
                 title={item.title}
                 description={
                   <div className={styles.dataRows}>
-                    <DataRow
-                      icon='environment'
-                      theme='outlined'
-                      text={`${item.address.streetName}`}
-                    />
-                    <DataRow icon='clock-circle' theme='outlined' text={`${item.time}`} />
-                    <DataRow
-                      icon='dollar'
-                      theme='outlined'
-                      text={item.isFree ? 'Gratuito' : 'Pagado'}
-                    />
+                    <Text strong>{item.subtitle}</Text>
+                    <DataRow icon='calendar' theme='outlined' text={`${item.publishedAt}`} />
                   </div>
                 }
               />
@@ -87,4 +80,4 @@ const Events: React.FC = () => {
   )
 }
 
-export default Events
+export default News
